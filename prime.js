180b558d16573d8d09121e04e99ca701a1208c4d
rahul raman <rahulraman707073@gmail.com>
2022-12-08 15:04:35 +0530

    code fixxed by rahul

--- prime.js
+++ prime.js
@@ -1,8 +1,12 @@
 let n = 9;
+let count =0;
 for(let i=2; i<=n/2; i++){
   if(n%i==0){
-    console.log("no")
-    return;
+    count++;
   }
 }
-console.log("yes")
+if(count===0){
+console.log("yes");
+}else{
+console.log("No");
+}
